671dcd91a5ca642568da1294cff4cc36ac3cf891
Marcin Szymański <ms32035@gmail.com>
2024-09-01 16:07:20 +0100

    add put function and make get public

--- ghostfolio/__init__.py
+++ ghostfolio/__init__.py
@@ -14,6 +14,11 @@ class Ghostfolio:
         self._jwt_token: str | None = None
         self._jwt_token_expiry: datetime | None = None
 
+    def _url(self, endpoint: str, object_id: str = None, api_version: str = "v1"):
+        return f"{self.host}/api/{api_version}/{endpoint}/" + (
+            object_id + "/" if object_id else ""
+        )
+
     def _refresh_jwt_token(self):
         if self._jwt_token is not None and self._jwt_token_expiry < datetime.now():
             return
@@ -25,23 +30,38 @@ class Ghostfolio:
         )["authToken"]
         self._jwt_token_expiry = datetime.now() + timedelta(days=30)
 
-    def _get(self, endpoint: str, params=None, api_version: str = "v1"):
+    def get(self, endpoint: str, params=None, api_version: str = "v1"):
         self._refresh_jwt_token()
 
         return self._process_response(
             requests.get(
-                f"{self.host}/api/{api_version}/{endpoint}/",
+                self._url(endpoint, api_version=api_version),
                 headers={"Authorization": f"Bearer {self._jwt_token}"},
                 params=params,
             )
         )
 
-    def post(self, endpoint: str, data=None, api_version: str = "v1", object_id: str = None):
+    def post(
+        self, endpoint: str, data=None, api_version: str = "v1", object_id: str = None
+    ):
         self._refresh_jwt_token()
 
         return self._process_response(
             requests.post(
-                f"{self.host}/api/{api_version}/{endpoint}/" + (object_id + "/" if object_id else ""),
+                self._url(endpoint, object_id, api_version),
+                headers={"Authorization": f"Bearer {self._jwt_token}"},
+                json=data,
+            )
+        )
+
+    def put(
+        self, endpoint: str, data=None, api_version: str = "v1", object_id: str = None
+    ):
+        self._refresh_jwt_token()
+
+        return self._process_response(
+            requests.put(
+                self._url(endpoint, object_id, api_version),
                 headers={"Authorization": f"Bearer {self._jwt_token}"},
                 json=data,
             )
@@ -60,19 +80,19 @@ class Ghostfolio:
     def orders(self, account_id: str | None = None) -> dict:
         """Get all orders."""
         params = {"accounts": account_id} if account_id else None
-        return self._get("order", params=params)
+        return self.get("order", params=params)
 
     def performance(self, date_range: str = "max") -> dict:
-        return self._get(
+        return self.get(
             "portfolio/performance", params={"range": date_range}, api_version="v2"
         )
 
     def holdings(self, date_range: str = "max") -> dict:
-        return self._get("portfolio/holdings", params={"range": date_range})
+        return self.get("portfolio/holdings", params={"range": date_range})
 
     def position(self, data_source: str, symbol: str):
         """Get position for a symbol from a data source."""
-        return self._get(f"portfolio/position/{data_source}/{symbol}")
+        return self.get(f"portfolio/position/{data_source}/{symbol}")
 
     def import_transactions(self, data: dict):
         """Import transactions."""
@@ -80,30 +100,30 @@ class Ghostfolio:
 
     def details(self) -> dict:
         """Get all details, including accounts, positions, and summary."""
-        return self._get("portfolio/details")
+        return self.get("portfolio/details")
 
     def investments(self, group_by: str = "month", date_range: str = "max") -> dict:
         """Get investments grouped by period."""
-        return self._get(
+        return self.get(
             "portfolio/investments", params={"range": date_range, "groupBy": group_by}
         )
 
     def dividends(self, group_by: str = "month", date_range: str = "max") -> dict:
         """Get dividends grouped by period."""
-        return self._get(
+        return self.get(
             "portfolio/dividends", params={"range": date_range, "groupBy": group_by}
         )
 
     def accounts(self) -> dict:
-        return self._get("account")
+        return self.get("account")
 
     def market_data_admin(self) -> dict:
         """Overview of market data loaded"""
-        return self._get("admin/market-data")
+        return self.get("admin/market-data")
 
     def market_data(self, data_source: str, symbol: str):
         """Get market data for a symbol from a data source."""
-        return self._get(f"admin/market-data/{data_source}/{symbol}")
+        return self.get(f"admin/market-data/{data_source}/{symbol}")
 
     def __hash__(self) -> int:
         return hash((self.token, self.host))
